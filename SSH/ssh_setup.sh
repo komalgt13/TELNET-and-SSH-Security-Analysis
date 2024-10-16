bef962363eb8d1e6a2fa13429d681d3ce94756ff
komalgt13 <komal1@cs.iitr.ac.in>
2024-10-16 11:09:07 +0530

    Improving User Experience

--- SSH/ssh_setup.sh
+++ SSH/ssh_setup.sh
@@ -4,47 +4,64 @@
 sudo apt update
 sudo apt upgrade
 
-# Install OpenSSH server
-sudo apt install openssh-server -y
+echo "Choose an option:"
+echo "1. Setup SSH Server"
+echo "2. Connect using SSH"
+echo "3. Change configuration for SSH server"
+read -p "Enter your choice : " choice
 
-# Enable and start the SSH service
-sudo systemctl enable --now ssh
-sudo systemctl start ssh
+if [ "$choice" -eq 1 ]; then
 
-#configure firewall
-sudo ufw allow ssh
 
-# Check the status of the SSH service
-sudo systemctl status ssh
+    # Install OpenSSH server
+    sudo apt install openssh-server -y
 
-#create a backup config file
-sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.initial
+    # Enable and start the SSH service
+    sudo systemctl enable --now ssh
+    sudo systemctl start ssh
 
-#edit the file for enhanced protection
-sudo nano /etc/ssh/sshd_config
+    #configure firewall
+    sudo ufw allow ssh
 
-#change port number from 22 to temporary port eg 49532
-#turn PubkeyAuthentication yes
-#turn passwordAuthentication yes
+    # Check the status of the SSH service
+    sudo systemctl status ssh
 
-#generate passkey for auth
-#ssh-keygen -t rsa -b 4096
-#ssh-copy-id username@localhost
+    #create a backup config file
+    sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.initial
 
-#restart the service
-sudo systemctl status ssh
-sudo systemctl start ssh
+    #change port number from 22 to temporary port eg 49532
+    #turn PubkeyAuthentication yes
+    #turn passwordAuthentication yes
 
-#chmod 700 ~/.ssh
-#chmod 600 ~/.ssh/id_rsa
-#chmod 644 ~/.ssh/id_rsa.pub
+    #generate passkey for auth
+    #ssh-keygen -t rsa -b 4096
+    #ssh-copy-id username@localhost
 
-echo "SSH server setup completed. You can connect using 'ssh <username>@localhost'."
+    #restart the service
+    sudo systemctl status ssh
+    sudo systemctl start ssh
 
-echo "User who wants to log in."
-read user
+    #chmod 700 ~/.ssh
+    #chmod 600 ~/.ssh/id_rsa
+    #chmod 644 ~/.ssh/id_rsa.pub
 
-echo "IP address you want to log into."
-read ip
+    echo "SSH server setup completed. You can connect using 'ssh <username>@localhost'."
 
-ssh $user@$ip
+elif [ "$choice" -eq 2 ]; then
+
+    echo "User who wants to log in."
+    read user
+
+    echo "IP address you want to log into."
+    read ip
+
+    ssh $user@$ip
+
+elif [ "$choice" -eq 3 ]; then
+    #edit the file for enhanced protection
+    sudo nano /etc/ssh/sshd_config
+    
+else
+    echo "Invalid choice. Please run the script again and choose 1 or 2."
+    exit 1
+fi
--- TELNET/telnet_setup.sh
+++ TELNET/telnet_setup.sh
@@ -4,28 +4,69 @@
 sudo apt update
 sudo apt upgrade
 
-#install telnet server
-sudo apt install telnetd xinetd -y
+echo "Choose an option:"
+echo "1. Setup Telnet Server"
+echo "2. Connect using Telnet"
+echo "3. Edit config file"
+read -p "Enter your choice : " choice
 
-#check the status of xinetd service
-#press 'q' to exit
-sudo systemctl status xinetd.service 
+if [ "$choice" -eq 1 ]; then
 
-#create the config file
-sudo nano /etc/xinetd.d/telnet
 
-#restart the service
-sudo systemctl restart xinetd.service
+    #install telnet server
+    sudo apt install telnetd xinetd -y
 
-#open port 23
-sudo ufw allow 23
+    #check the status of xinetd service
+    #press 'q' to exit
+    sudo systemctl status xinetd.service 
 
-#install telnet client
-sudo apt install telnet
+    #create the config file
+    cat > filename.txt << EOF
+        service telnet
 
-echo "Telnet server setup completed."
+        {
 
-echo "IP address you want to log into."
-read ip
+        disable = no
 
-telnet $ip
+        flags = REUSE
+
+        socket_type = stream
+
+        wait = no
+
+        user = root
+
+        server = /usr/sbin/in.telnetd
+
+        log_on_failure += USERID
+
+        }
+EOF
+
+    sudo nano /etc/xinetd.d/telnet
+
+    #restart the service
+    sudo systemctl restart xinetd.service
+
+    #open port 23
+    sudo ufw allow 23
+
+    echo "Telnet server setup completed."
+elif [ "$choice" -eq 2 ]; then
+
+    #install telnet client
+    sudo apt install telnet
+
+
+    echo "IP address you want to log into."
+    read ip
+
+    telnet $ip
+elif [ "$choice" -eq 3 ]; then
+    sudo nano /etc/xinetd.d/telnet 
+
+else
+    echo "Invalid choice. Please run the script again and choose 1 or 2."
+    exit 1
+
+fi
